ccc95499f5c33167ffb7a5cc42715189606f592a
syed farhan <syedfarhan9034@gmail.com>
2021-05-29 17:04:57 +0530

    Added Admin Home page and custom CSS file

---
+++ Web/Quizizz.Web/Areas/Administration/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿namespace Quizizz.Web.Areas.Administration.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Quizizz.Web.ViewModels.Quizzes;
+
+    public class HomeController : AdministrationController
+    {
+        public IActionResult Index(string password, string errorText)
+        {
+            var model = new PasswordInputViewModel();
+            if (errorText != null)
+            {
+                model.Password = password;
+                model.Error = errorText;
+            }
+
+            return this.View(model);
+        }
+    }
+}
