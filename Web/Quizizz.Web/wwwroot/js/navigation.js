962a7d7adc2f2c3ddbadcba2adaa02763ae0c891
syed farhan <syedfarhan9034@gmail.com>
2021-05-31 23:47:40 +0530

    fixed some bugs for dashboard UI

---
+++ Web/Quizizz.Web/wwwroot/js/navigation.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+    const URL_PARTS = {
+        STUDENTS: "StudentsAll",
+        GROUPS: "GroupsAll",
+        RESULTS: "ResultsAll",
+        QUIZZES: "QuizzesAll",
+        Events: "EventsAll",
+        MAIN: "Dashboard"
+    }
+
+    var url = window.location.href;
+    var navs = $('a[class*=" nav-to-change"]');
+
+    [...navs].forEach(x => {
+        $(x).removeClass("active");
+    })
+
+    if (url.indexOf(URL_PARTS.RESULTS) >= 0) {
+        $('#results').addClass('active');
+    } else if (url.indexOf(URL_PARTS.EVENTS) >= 0) {
+        $('#events').addClass('active');
+    } else if (url.indexOf(URL_PARTS.QUIZZES) >= 0) {
+        $('#quizzes').addClass('active');
+    } else if (url.indexOf(URL_PARTS.GROUPS) >= 0) {
+        $('#groups').addClass('active');
+    } else if (url.indexOf(URL_PARTS.STUDENTS) >= 0) {
+        $('#students').addClass('active');
+    } else if (url.indexOf(URL_PARTS.MAIN) >= 0) {
+        $('#main').addClass('active');
+    }
+})
